3b06f9fe1e680f750c2d037617756e4f664bac92
k1rer <ghas_00@mail.ru>
2026-02-28 23:11:23 +0700

    add 2 solution

---
+++ Арифметика действительных чисел/Sakharchuk3/Sakharchuk3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    float x, y, z;
+    cin >> x >> y >> z;
+    float a = (1 + pow(sin(x + y), 2)) / (2 + abs(x - (2 * x) / (1 + pow(x, 2) * pow(y, 2)))) + 1;
+    float b = pow(cos(atan(1 / z)), 2);
+    cout << a << endl;
+    cout << b;
+}
