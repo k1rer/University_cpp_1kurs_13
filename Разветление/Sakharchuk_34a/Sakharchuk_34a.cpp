3e2d817f22d44b9b03d1c642e4325c79ba64a504
k1rer <ghas_00@mail.ru>
2026-05-06 17:13:46 +0700

    Добавлены задания 40, 60В, 34а

---
+++ Разветление/Sakharchuk_34a/Sakharchuk_34a.cpp
@@ -0,0 +1,15 @@
+﻿//34. Даны действительные числа x, y, z.Получить:
+//а) max(x, y, z);
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"ru");
+    double x, y, z;
+    cin >> x >> y >> z;
+    auto res = max(max(x, y),z);
+    cout << "Максимальное число: " << res;
+}
